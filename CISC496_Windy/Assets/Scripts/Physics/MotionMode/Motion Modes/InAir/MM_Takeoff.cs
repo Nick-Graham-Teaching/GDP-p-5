48a4ad36d9035987283dceff15419a1a2fcc8498
RinT745263 <387805946@qq.com>
2023-03-25 21:14:53 -0400

    Music

--- CISC496_Windy/Assets/Scripts/Physics/MotionMode/Motion Modes/InAir/MM_Takeoff.cs
+++ CISC496_Windy/Assets/Scripts/Physics/MotionMode/Motion Modes/InAir/MM_Takeoff.cs
@@ -11,7 +11,7 @@ namespace Windy.MotionMode {
 
         IEnumerator IsTakingOffSuccessful()
         {
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(2.0f);
             if (MM_Executor.Instance.MotionMode.GetType() == MM_Executor.Instance.B_M_Takeoff.Build().GetType())
             {
                 MM_Executor.Instance.StopEnergySupervisor();
